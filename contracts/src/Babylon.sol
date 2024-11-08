aa5edcd94018a7146cf98a6fe5044de98db6d42a
emanherawy <eman.herawy@gmaail.com>
2024-11-08 01:52:31 +0100

    add babylone fake token

---
+++ contracts/src/Babylon.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Babylon is ERC20{
+
+    constructor() ERC20("Babylon", "BBL") {
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+}
