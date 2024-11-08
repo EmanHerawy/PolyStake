0032ce6ffb77317ddf82bf6648fc0985b454aadc
emanherawy <eman.herawy@gmaail.com>
2024-11-08 03:54:19 +0100

    update script

--- contracts/script/poolManagerDeploy.s.sol
+++ contracts/script/poolManagerDeploy.s.sol
@@ -2,12 +2,12 @@
 pragma solidity 0.8.26;  // Updated to match PoolManager version
 
 import "forge-std/Script.sol";
-import "../src/PoolManager.sol";
-import "../src/NoDelegateCall.sol";
-import "../src/ProtocolFees.sol";
-import "../src/ERC6909Claims.sol";
-import "../src/Extsload.sol";
-import "../src/Exttload.sol";
+import {PoolManager} from "@uniswap/v4-core/src/PoolManager.sol";
+// import "../src/NoDelegateCall.sol";
+// import "../src/ProtocolFees.sol";
+// import "../src/ERC6909Claims.sol";
+// import "../src/Extsload.sol";
+// import "../src/Exttload.sol";
 
 //   PoolManager deployed at: 0x0F9CAA931B6fe71e5380163ba20E618Af82CBAc9
 
